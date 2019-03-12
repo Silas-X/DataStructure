bf79b9e14e842dda6221665992903876d5189662
Silas XU <Silas_X@outlook.com>
2019-03-12 14:38:04 +0800

    Binary_Tree definition and Interface

--- DataStructures/TreeStructures/TreeTemplate.h
+++ DataStructures/TreeStructures/TreeTemplate.h
@@ -5,8 +5,33 @@ class Binary_tree {
  public:
   Binary_tree();
 
+  // accessor function
+  bool empty() const;
+  int size() const;
+
+  // mutator function
+  void clear();
+  int height() const;
+  void insert(const Entry &);
+
+  // traversal
+  void preorder(void (*visit)(Entry &));
+  void inorder(void (*visit)(Entry &));
+  void postorder(void (*visit)(Entry &));
+
+  // constructor
+  Binary_tree(const Binary_tree<Entry> &origin);  // copy constructor
+  ~Binary_tree();                                 // deconstructor
+
+  // operation overloding
+  Binary_tree &operator=(const Binary_tree<Entry> &otigin);
+
  private:
   struct Binary_node<Entry> root;
+  void recursive_preorder(Binary_node<Entry> *sub_root, void (*visit)(Entry &));
+  void recursive_inorder(Binary_node<Entry> *sub_root, void (*visit)(Entry &));
+  void recursive_postorder(Binary_node<Entry> *sub_root,
+                           void (*visit)(Entry &));
 };
 
 template <class Entry>
@@ -19,9 +44,3 @@ struct Binary_node {
   Binary_node();
   Binary_node(const Entry &x);
 };
-
-template <class Entry>
-Binary_tree<Entry>::Binary_tree() {
-  root = NULL;
-};
-
